8a1a3cdbf0fe97fcf03a1ec63ad2861da04195d0
paul-krug <paul_konstantin.krug@tu-dresden.de>
2024-07-25 16:19:39 +0200

    initial commit

---
+++ target_approximation/vocaltractlab/utils.py
@@ -0,0 +1,47 @@
+
+
+supraglottal_tiers = dict(
+    default =[
+        'HX',
+        'HY',
+        'JX',
+        'JA',
+        'LP',
+        'LD',
+        'VS',
+        'VO',
+        'TCX',
+        'TCY',
+        'TTX',
+        'TTY',
+        'TBX',
+        'TBY',
+        'TRX',
+        'TRY',
+        'TS1',
+        'TS2',
+        'TS3',
+        ]
+    )
+
+glottal_tiers = dict(
+    default = [
+        'F0',
+        'PR',
+        'XB',
+        'XT',
+        'CA',
+        'PL',
+        'RA',
+        'DP',
+        'PS',
+        'FL',
+        'AS',
+        ]
+    )
+
+ms_file_extensions = [
+    '.yaml',
+    '.yaml.gz',
+    '.ms',
+    ]

097550022dec2e79af76571c4c27265681d6cd0d
paul-krug <paul_konstantin.krug@tu-dresden.de>
2024-07-26 16:52:56 +0200

    include all packages

--- setup.py
+++ setup.py
@@ -10,13 +10,17 @@ install_requires = [
 
 setup(
     name='target-approximation',
-    version='0.0.1',
+    version='0.0.2',
     description='Python implementation of the Target-Approximation-Model.',
     author='Paul Krug',
     url='https://github.com/paul-krug/target-approximation',
     license='MIT',
     long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
-    packages=['target_approximation'],
+    packages=[
+        'target_approximation',
+        'target_approximation.vocaltractlab',
+        'target_approximation.tensortract',
+        ],
     install_requires=install_requires
 )

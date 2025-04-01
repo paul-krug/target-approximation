bbfd25406490f7cdb041b7edadd22d3bc08d3669
paul-krug <paul_konstantin.krug@tu-dresden.de>
2025-04-01 19:46:30 +0200

    update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ install_requires = [
 
 setup(
     name='target-approximation',
-    version='0.0.4',
+    version='0.0.5',
     description='Python implementation of the Target-Approximation-Model.',
     author='Paul Krug',
     url='https://github.com/paul-krug/target-approximation',

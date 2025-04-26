868e94a9d9dfaa17ef513d0fe2669160e4c38dfe
paul-krug <paul_konstantin.krug@tu-dresden.de>
2025-04-26 20:23:32 +0200

    get_file_type

--- target_approximation/utils.py
+++ target_approximation/utils.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from tools_io import check_if_list_is_valid
 from tools_io import make_output_path
 from tools_io import is_iterable
+import yaml
 
 
 
@@ -166,3 +167,107 @@ def make_path( path ):
             exist_ok=True,
             )
     return
+
+def get_file_type( file_path ):
+    """
+    This function returns the file type of a file. It reads the file and tries 
+    to determine the file type based on the content.
+    """
+    # First try to read the file as a yaml file
+    try:
+        ft = file_type_from_meta_data( file_path )
+    except ValueError:
+        # The file is either not a yaml file or does not contain meta data.
+        try:
+            ft = is_tract_seq_file( file_path )
+        except ValueError:
+            raise ValueError(
+                f"""
+                An error occured while trying to infer the file type of file: {file_path}.
+                The file is not a valid tract sequence file and does not contain meta data.
+                """
+                )
+    return ft
+    
+def file_type_from_meta_data( file_path ):
+    try:
+        with open( file_path, 'r' ) as f:
+            x = yaml.load( f, Loader = yaml.FullLoader )
+    except Exception:
+        raise ValueError(
+            f"""
+            An error occured while trying to read the file: {file_path} as a yaml file.
+            The file might be broken or is not a yaml file.
+            """
+            )
+    # x is a dictionary
+    # check if key "meta_data" exists in x
+    if 'meta_data' in x.keys():
+        if 'file_type' in x['meta_data'].keys():
+            ft = x['meta_data']['file_type']
+        else:
+            raise ValueError(
+                f"""
+                An error occured While trying to infer the file type of file: {file_path}.
+                The file contains the key 'meta_data' but 'file_type' does not exist in the
+                dictionary. The file might be broken, please check the file and try again.
+                """
+                )
+    else:
+        # file does not contain the key 'meta_data'
+        # File might be an older version of the file format (from target_approximation<=0.0.4)
+        # For backwards compatibility, we could check if the file is a target sequence or series file
+        # directly, by reading the contents of the yaml structure. But for now, we break backwards
+        # compatibility and raise an error.
+        raise ValueError(
+            f"""
+            The yaml file: {file_path} does not contain the key 'meta_data'.
+            The file might be an older version of the file format (from target_approximation<=0.0.5).
+            From version 0.0.6 onwards, the file type is no longer infered based on the content of the
+            file. If you want to load the file, please update the yaml file like this:
+
+            meta_data:
+                file_type: <file_type>
+            data:
+                <data>
+
+            where <file_type> is one of the following:
+            - target_sequence
+            - target_series
+            - <vtl/tt>_motor_<series/sequence>
+            - <vtl/tt>_supraglottal_<series/sequence>
+            - vtl_tractseq_file
+
+            and data is the data of the file, just like before.
+            """
+            )
+        #if 'series' in x.keys() and 'sr' in x.keys():
+        #    if set( x[ 'series' ].keys() ) == set( vtl_tiers ):
+        #        ft = 'vtl_motor_series'
+        #    elif set( x[ 'series' ].keys() ) == set( tt_tiers ):
+        #        ft = 'tt_motor_series'
+        #    elif set( x[ 'series' ].keys() ) == set( vtl_sg_tiers[ 'default' ] ):
+        #        ft = 'vtl_supraglottal_series'
+        #    elif set( x[ 'series' ].keys() ) == set( tt_sg_tiers[ 'jd3' ] ):
+        #        ft = 'tt_supraglottal_series'
+        #    else:
+        #        ft = 'unknown'
+        #else:
+
+    return ft
+
+def is_tract_seq_file( file_path ):
+    # Check if the file is a tract sequence file ( normal text file )
+    # Read the first line and compare against a reference line
+    with open( file_path, 'r' ) as f:
+        line = f.readline()
+    if line == TRACT_SEQ_FIRST_LINE:
+        ft = 'vtl_tractseq_file'
+    else:
+        raise ValueError(
+            f"""
+            The file: {file_path} is not a valid tract sequence file.
+            The first line of the file does not match the reference line.
+            """
+            )
+    return ft
